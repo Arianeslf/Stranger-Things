0f457d5dededd2de33c5ba81603afaf19ed4ec63
Ariane Silvestre <arianesilvetremaira73@gmail.com>
2026-06-18 21:55:58 -0300

    modified 3 files in .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 ScrollSmoother.create({
@@ -5,54 +6,98 @@ ScrollSmoother.create({
   effects: true,
 });
 
-gsap.from(".hero", {
-  opacity: 0,
-  duration: 1,
-});
+function animarPagina() {
 
-gsap.from("picture:nth-child(2)", {
-  y: 200,
-  duration: 1,
-});
+  gsap.from(".hero", {
+    opacity: 0,
+    duration: 1,
+  });
 
-gsap.from("picture:nth-child(1)", {
-  y: -60,
-  duration: 1,
-});
+  gsap.from("picture:nth-child(2)", {
+    y: 60,
+    duration: 1,
+  });
 
-gsap.from(".card", {
-  opacity: 0,
-  duration: 1,
-  filter: "blur(10px",
-  stagger: 0.3,
-  scrollTrigger: {
-    trigger: ".cards",
-    end: "100% 70%",
-    start: "0% 100%",
-    scrub: true,
+  gsap.from("picture:nth-child(1)", {
+    y: -60,
+    duration: 1,
+  });
+
+  gsap.from(".card", {
+    opacity: 0,
+    filter: "blur(10px)",
+    stagger: 0.3,
+    scrollTrigger: {
+      trigger: ".cards",
+      start: "0% 80%",
+      end: "100% 70%",
+      scrub: true,
+    },
+  });
+
+  gsap.from(".secaoObrigado ul li", {
+    opacity: 0,
+    x: 40,
+    filter: "blur(10px)",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".secaoObrigado ul",
+      start: "0% 80%",
+      end: "100% 50%",
+      scrub: true,
+    },
+  });
+
+  gsap.from("footer", {
+    y: "-30%",
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: "footer",
+      scrub: true,
+      invalidateOnRefresh: true,
+      end: "100% 100%",
+    },
+  });
+
+  const grupoTextoSplit = document.querySelectorAll(".textoSplit");
+
+
+  grupoTextoSplit.forEach((textoUnicoSplit) => {
+    const split = SplitText.create(textoUnicoSplit, {
+      type: "lines, words, chars",
+      mask: "lines",
+    });
+
+    gsap.from(split.chars, {
+      y: 40,
+      opacity: 0,
+      duration: 0.3,
+      stagger: 0.03,
+      scrollTrigger: {
+        trigger: textoUnicoSplit,
+      },
+    });
+  });
+}
+
+
+
+const tl = gsap.timeline({
+  onComplete() {
+    animarPagina()
+    gsap.to("#preloader", {
+      opacity: 0,
+      display: "none",
+    });
   },
 });
 
-gsap.from(".secaoObrigado ul li", {
-  opacity: 0,
-  x: 40,
-  filter: "blur(10px",
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: ".secaoObrigado ul",
-    start: "0% 80%",
-    end: "100% 50%",
-    scrub: true,
-  },
+tl.to("#preloader path", {
+  duration: 1,
+  strokeDashoffset: 0,
 });
-
-gsap.from("footer", {
-  y: "-30%",
-  immediateRender: true,
-  scrollTrigger: {
-    trigger: "footer",
-    scrub: true,
-    invalidateOnRefresh: true,
-    end: "100% 100%",
-  },
+tl.to("#preloader path", {
+  fill: "rgb(168, 19, 19)",
+  duration: 0.5,
+  strokeDashoffset: 0,
 });
